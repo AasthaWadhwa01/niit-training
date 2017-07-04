42c6c9405c4a6a46b1aa252e5f207313e07ea67f
AasthaWadhwa01 <aasthawadhwa01@gmail.com>
2017-07-04 16:48:35 +0530

    Add files via upload

---
+++ Demostatic.java
@@ -0,0 +1,20 @@
+class Demostatic{
+static int count=0;
+
+
+public static void main(String args[])
+{
+
+Demostatic a=new Demostatic();
+count++;
+System.out.println(count);
+
+Demostatic b=new Demostatic();
+count++;
+System.out.println(count);
+
+Demostatic c=new Demostatic();
+count++;
+System.out.println(count);
+}
+}
